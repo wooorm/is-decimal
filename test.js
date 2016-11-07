2193b75c57aefdee294497e8844eb93e032124af
Titus Wormer <tituswormer@gmail.com>
2016-11-07 20:33:57 +0100

    Remove superfluous comments

--- test.js
+++ test.js
@@ -1,20 +1,8 @@
-/**
- * @author Titus Wormer
- * @copyright 2016 Titus Wormer
- * @license MIT
- * @module is-decimal
- * @fileoverview Test suite for `is-decimal`.
- */
-
 'use strict';
 
-/* eslint-env node */
-
-/* Dependencies. */
 var test = require('tape');
 var decimal = require('./');
 
-/* Tests. */
 test('decimal(character)', function (t) {
   t.ok(decimal('0'));
   t.ok(decimal('9'));
