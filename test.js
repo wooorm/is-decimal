040e2ace3c66759acb52f1b809eccc935e273aa5
Titus Wormer <tituswormer@gmail.com>
2018-04-21 12:05:23 +0200

    Refactor code-style

--- test.js
+++ test.js
@@ -1,17 +1,17 @@
-'use strict';
+'use strict'
 
-var test = require('tape');
-var decimal = require('.');
+var test = require('tape')
+var decimal = require('.')
 
-test('decimal(character)', function (t) {
-  t.ok(decimal('0'));
-  t.ok(decimal('9'));
-  t.ok(decimal('1'.charCodeAt(0)));
-  t.notOk(decimal('a'));
-  t.notOk(decimal('0'.charCodeAt(0) - 1));
-  t.notOk(decimal('9'.charCodeAt(0) + 1));
-  t.notOk(decimal('F'.charCodeAt(0)));
-  t.notOk(decimal('💩'));
+test('decimal(character)', function(t) {
+  t.ok(decimal('0'))
+  t.ok(decimal('9'))
+  t.ok(decimal('1'.charCodeAt(0)))
+  t.notOk(decimal('a'))
+  t.notOk(decimal('0'.charCodeAt(0) - 1))
+  t.notOk(decimal('9'.charCodeAt(0) + 1))
+  t.notOk(decimal('F'.charCodeAt(0)))
+  t.notOk(decimal('💩'))
 
-  t.end();
-});
+  t.end()
+})
