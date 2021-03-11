97ad34a50d39fbcc42b7e62a58c6325df507cd6a
Titus Wormer <tituswormer@gmail.com>
2021-03-11 09:43:53 +0100

    Add JSDoc based types

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-// Check if the given character code, or the character code at the first
-// character, is decimal.
+/**
+ * Check if the given character code, or the character code at the first
+ * character, is decimal.
+ *
+ * @param {string|number} character
+ * @returns {boolean} Whether `character` is a decimal
+ */
 export function isDecimal(character) {
   var code = typeof character === 'string' ? character.charCodeAt(0) : character
 
