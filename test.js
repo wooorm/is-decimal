27417c3d19d4c2f36a0f6a6c75f5067ccc367b5d
Titus Wormer <tituswormer@gmail.com>
2022-11-23 12:31:12 +0100

    Use Node test runner

--- test.js
+++ test.js
@@ -1,19 +1,18 @@
-import test from 'tape'
+import assert from 'node:assert/strict'
+import test from 'node:test'
 import {isDecimal} from './index.js'
 
-test('isDecimal(character)', function (t) {
+test('isDecimal(character)', function () {
   const _0 = 0x30
   const _9 = 0x39
   const F = 0x46
 
-  t.ok(isDecimal('0'))
-  t.ok(isDecimal('9'))
-  t.ok(isDecimal(_0))
-  t.notOk(isDecimal('a'))
-  t.notOk(isDecimal(_0 - 1))
-  t.notOk(isDecimal(_9 + 1))
-  t.notOk(isDecimal(F))
-  t.notOk(isDecimal('💩'))
-
-  t.end()
+  assert.ok(isDecimal('0'))
+  assert.ok(isDecimal('9'))
+  assert.ok(isDecimal(_0))
+  assert.ok(!isDecimal('a'))
+  assert.ok(!isDecimal(_0 - 1))
+  assert.ok(!isDecimal(_9 + 1))
+  assert.ok(!isDecimal(F))
+  assert.ok(!isDecimal('💩'))
 })
